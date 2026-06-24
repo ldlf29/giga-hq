42b7f2f31a15bf0977b0342f0849e99e215438e5
ldlf29 <lucasdelafuente29@gmail.com>
2026-06-24 16:17:24 -0300

    chore: add ignore and scratch folders to gitignore and remove from tracked files

--- scratch/test_keccak.mjs
+++
@@ -1,5 +0,0 @@
-import { keccak256, toBytes } from 'viem';
-
-const signature = 'RaceCreated(uint256,address,uint256,uint256,uint256,uint256,uint256)';
-const hash = keccak256(toBytes(signature));
-console.log('Event Hash:', hash);
